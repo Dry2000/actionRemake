b43395516668d973fe9d27dcf0685b4884d7efc5
Dry2000 <e1625@s.akashi.ac.jp>
2019-05-13 16:36:16 +0900

    message

---
+++ actionRemake/GameScene.swift
@@ -0,0 +1,21 @@
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+    
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    
+    }
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
