27e3d8389013d5a60981568f27789528d1a8f5b5
Dry2000 <e1625@s.akashi.ac.jp>
2019-05-19 18:51:00 +0900

    message

--- actionRemake/GameScene.swift
+++ actionRemake/GameScene.swift
@@ -3,23 +3,52 @@ import SpriteKit
 import GameplayKit
 struct information{
     var hp:Int
-    var 
+    var inform:Int
+    var jumping:Bool
 }
 var player = SKSpriteNode(imageNamed:"player")
+let playerCategory:UInt32 = 0x0001
+let enemyCategory:UInt32 = 0x0010
+let blockCategory:UInt32 = 0x0100
 var enemy:[SKSpriteNode]=[]
-
-class GameScene: SKScene {
+var enemyInfo:[information]=[]
+var map:[SKSpriteNode]=[]
+var oneScale:CGFloat!
+class GameScene: SKScene,SKPhysicsContactDelegate{
     
     
     override func didMove(to view: SKView) {
-        
-    
+        self.physicsWorld.gravity = CGVector(dx:0,dy:-3.0)
+        self.physicsWorld.contactDelegate = self
+        oneScale = self.frame.width/15
+        player.xScale = oneScale/player.frame.width
+        player.yScale = oneScale/player.frame.height
+       // player.position = CGPoint(x:-self.frame.width+oneScale,y:-self.frame.height+2*oneScale)
+        player.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width:player.frame.width,height:player.frame.height))
+        player.physicsBody?.categoryBitMask = playerCategory
+        player.physicsBody?.contactTestBitMask = enemyCategory+blockCategory
+        player.physicsBody?.collisionBitMask = enemyCategory+blockCategory
+        addChild(player)
+        for i in 0...15{
+            let spot = SKSpriteNode(imageNamed:"block")
+            spot.xScale = oneScale/spot.frame.width
+            spot.yScale = oneScale/spot.frame.height
+            spot.position = CGPoint(x:-self.frame.width/2+(CGFloat(i)*oneScale),y:-2*oneScale)
+            spot.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width:spot.frame.width,height:spot.frame.height))
+            spot.physicsBody?.categoryBitMask = blockCategory
+            spot.physicsBody?.contactTestBitMask = enemyCategory+playerCategory
+            spot.physicsBody?.collisionBitMask = enemyCategory+playerCategory
+            spot.physicsBody?.isDynamic = false
+            addChild(spot)
+            map.append(spot)
+        }
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
     
     }
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        //print(player.position)
     }
+   
 }
