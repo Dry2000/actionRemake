40f37630f492f1936a52ca503fd413a59649e000
Dry2000 <e1625@s.akashi.ac.jp>
2019-06-22 22:51:03 +0900

    message

--- actionRemake/GameScene.swift
+++ actionRemake/GameScene.swift
@@ -1,16 +1,17 @@
 
 import SpriteKit
 import GameplayKit
+
 struct information{
     var hp:Int
     var inform:Int
-    var jumping:Bool
+    var isJumping:Bool
 }
 var player = SKSpriteNode(imageNamed:"player")
 let playerCategory:UInt32 = 0x0001
 let enemyCategory:UInt32 = 0x0010
 let blockCategory:UInt32 = 0x0100
-var enemy:[SKSpriteNode]=[]
+var enemies:[SKSpriteNode]=[]
 var enemyInfo:[information]=[]
 var map:[SKSpriteNode]=[]
 var leftArrow = SKSpriteNode(imageNamed:"leftArrow")
@@ -62,6 +63,27 @@ class GameScene: SKScene,SKPhysicsContactDelegate{
             addChild(spot)
             map.append(spot)
         }
+        for i in 0..<3{
+            let enemy = SKSpriteNode(imageNamed:"\(i)")
+            enemyInfo.append(information(hp:3,inform:2,isJumping:false))
+            enemy.position = CGPoint(x:Int(oneScale)*(5+i),y:Int(oneScale)*(3+i))
+            enemy.xScale = oneScale/enemy.frame.width
+            enemy.yScale = oneScale/enemy.frame.height
+            enemy.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width:enemy.frame.width,height:enemy.frame.height))
+            enemy.physicsBody?.categoryBitMask = enemyCategory
+            enemy.physicsBody?.contactTestBitMask = blockCategory+playerCategory
+            enemy.physicsBody?.collisionBitMask = blockCategory+playerCategory
+            if enemyInfo[i].inform == 2{
+                enemy.physicsBody?.isDynamic = false
+            }else{
+                enemy.physicsBody?.isDynamic = true
+            }
+            let move=SKAction.moveTo(x:-frame.width/2,duration:10.0)
+            let remove=SKAction.removeFromParent()
+            enemy.run(SKAction.sequence([move,remove]))
+            enemies.append(enemy)
+            self.addChild(enemy)
+        }
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches{
@@ -69,16 +91,7 @@ class GameScene: SKScene,SKPhysicsContactDelegate{
             let touchedNode = self.atPoint(location)
             print(touchedNode)
             if touchedNode == upArrow && !isJumping{
-               /* // move up 20
-                let jumpUpAction = SKAction.moveBy(x:0,y:oneScale*2.5,duration:0.5)
-                // move down 20
-               // let jumpDownAction = SKAction.moveBy(x:0,y:-oneScale*2.5,duration:0.2)
-                // sequence of move yup then down
-                let jumpSequence = SKAction.sequence([jumpUpAction/*, jumpDownAction*/])
-                
-                // make player run sequence
-                player.run(jumpSequence)
- */             player.physicsBody?.velocity.dy = oneScale*5
+            player.physicsBody?.velocity.dy = oneScale*5
             }
             if touchedNode == leftArrow{
                 isLeft = true
@@ -104,16 +117,46 @@ class GameScene: SKScene,SKPhysicsContactDelegate{
         }
     }
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        isLeft = false
-        isRight = false
+        for touch in touches {
+            let location = touch.location(in: self)
+            let touchedNode = self.atPoint(location)
+            if touchedNode == upArrow{
+                return
+            }
+            isLeft = false
+            isRight = false
+        }
+    }
+    func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.categoryBitMask == enemyCategory && contact.bodyB.categoryBitMask == playerCategory{
+            gameover()
+        }else if contact.bodyB.categoryBitMask == enemyCategory && contact.bodyA.categoryBitMask == playerCategory{
+            gameover()
+        }
+        if contact.bodyA.categoryBitMask == enemyCategory && contact.bodyB.categoryBitMask == blockCategory{
+            for i in 0..<enemies.count{
+                if enemies[i].position == contact.bodyA.node!.position && enemyInfo[i].inform == 2{
+                    contact.bodyB.node?.removeFromParent()
+                }
+            }
+        }else if contact.bodyB.categoryBitMask == enemyCategory && contact.bodyA.categoryBitMask == blockCategory{
+            for i in 0..<enemies.count{
+                if enemies[i].position == contact.bodyB.node!.position && enemyInfo[i].inform == 2{
+                    contact.bodyA.node?.removeFromParent()
+                }
+            }
+        }
+    }
+    func gameover(){
+        isPaused = true
+        print("GameOver")
+        let gameover = SKLabelNode(text:"Game Over")
+        gameover.fontName = "PixelMplus12-Bold"
+        self.addChild(gameover)
     }
     override func update(_ currentTime: TimeInterval) {
         if player.position.y < -frame.height/2{
-            isPaused = true
-            print("GameOver")
-            let gameover = SKLabelNode(text:"Game Over")
-            gameover.fontName = "PixelMplus12-Bold"
-            self.addChild(gameover)
+            gameover()
         }
         if(player.position.y==playerYpos){
             isJumping = false
@@ -126,11 +169,25 @@ class GameScene: SKScene,SKPhysicsContactDelegate{
             for i in 0..<map.count{
                 map[i].run(moveLeft)
             }
+            for i in 0..<enemies.count{
+                enemies[i].run(moveLeft)
+            }
         }
         let moveRight = SKAction.move(by:CGVector(dx:-oneScale/12,dy:0), duration: 0.5)
         if isRight{
             for i in 0..<map.count{
                 map[i].run(moveRight)
+            }
+            for i in 0..<enemies.count{
+                enemies[i].run(moveRight)
+            }
+        }
+        for i in 0..<enemies.count{
+            if enemyInfo[i].inform == 1 && abs(Int(player.position.x - enemies[i].position.x))<Int(oneScale)*2 && !enemyInfo[i].isJumping{
+                enemies[i].physicsBody?.velocity.dy = oneScale*5
+            }
+            if enemyInfo[i].inform == 2 && abs(Int(player.position.x - enemies[i].position.x))<Int(oneScale)*2{
+                enemies[i].physicsBody?.isDynamic = true
             }
         }
     }
